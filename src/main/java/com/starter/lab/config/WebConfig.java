1f5a67e3475c92b4c0bcfa1b7cc0dab337c11a12
sujoy99 <scg.sujoy@gmail.com>
2023-12-23 14:04:19 +0600

    image display for windows platform

--- src/main/java/com/starter/lab/config/WebConfig.java
+++ src/main/java/com/starter/lab/config/WebConfig.java
@@ -5,11 +5,14 @@ import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private static String os = System.getProperty("os.name").toLowerCase();
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -28,7 +31,13 @@ public class WebConfig implements WebMvcConfigurer {
          */
         registry.addResourceHandler("/" + dirName + "/**")
 //                .addResourceLocations("/"+ dirName + "/");
-                .addResourceLocations("file://" + uploadPath +"/");
+//                .addResourceLocations("file://" + uploadPath +"/");     // for linux
+//                .addResourceLocations("file:///" + uploadPath + "\");   // for windows
+                .addResourceLocations( osPlatform() + uploadPath + File.separator);
 
+    }
+
+    private String osPlatform(){
+        return os.contains("win") ? "file:///" : "file://";
     }
 }
